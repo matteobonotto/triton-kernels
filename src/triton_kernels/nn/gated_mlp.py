54dcc24a37e0b3a77649be69c3b61ae8d8cfefd5
matteobonotto <matteob.90@hotmail.it>
2025-11-14 16:14:24 +0000

    matmul

--- src/triton_kernels/nn/gated_mlp.py
+++ src/triton_kernels/nn/gated_mlp.py
@@ -1,17 +1,14 @@
-
-
-
-
 def mlp_fwd(x, W1, W2, W3):
     return (torch.nn.functional.silu(x @ W2.T) * (x @ W1.T)) @ W3.T
 
+
 class LlamaMLP(nn.Module):
-    def __init__(self, hidden_size:int = 64, intermediate_size:int = 256):
+    def __init__(self, hidden_size: int = 64, intermediate_size: int = 256):
         super().__init__()
         self.W1 = nn.Linear(hidden_size, intermediate_size, bias=False)
         self.W2 = nn.Linear(hidden_size, intermediate_size, bias=False)
         self.W3 = nn.Linear(intermediate_size, hidden_size, bias=False)
         self.act = nn.SiLU()
 
-    def forward(self, x:Tensor) -> Tensor:
+    def forward(self, x: Tensor) -> Tensor:
         return mlp_fwd(x, self.W1.weight, self.W2.weight, self.W3.weight)
--- tests/test_matmul.py
+++ tests/test_matmul.py
@@ -6,10 +6,62 @@ from triton_kernels.functional.matmul import matmul
 
 
 def test_matmul():
-    M, N, K = (10, 11, 12)
+    M, N, K = (10, 12, 14)
     A = torch.rand(M.N)
     B = torch.rand(N,K)
     
     out_ref = A @ B
     out = matmul(A,B)
     
+    BLOCK_SIZE_N, BLOCK_SIZE_M, GROUP_SIZE_M = (5, 4, 2)
+    import math
+    import numpy as np
+    
+    for pid in range((M // BLOCK_SIZE_M) * (N // BLOCK_SIZE_N)):
+        
+        """Kernel for computing the matmul C = A x B.
+        A has shape (M, K), B has shape (K, N) and C has shape (M, N)
+        """
+        # -----------------------------------------------------------
+        # Map program ids `pid` to the block of C it should compute.
+        # This is done in a grouped ordering to promote L2 data reuse.
+        # See above `L2 Cache Optimizations` section for details.
+        # pid = tl.program_id(axis=0)
+        num_pid_m = math.ceil(M, BLOCK_SIZE_M)
+        num_pid_n = math.ceil(N, BLOCK_SIZE_N)
+        num_pid_in_group = GROUP_SIZE_M * num_pid_n
+        group_id = pid // num_pid_in_group
+        first_pid_m = group_id * GROUP_SIZE_M
+        group_size_m = min(num_pid_m - first_pid_m, GROUP_SIZE_M)
+        pid_m = first_pid_m + ((pid % num_pid_in_group) % group_size_m)
+        pid_n = (pid % num_pid_in_group) // group_size_m
+
+        # ----------------------------------------------------------
+        # Create pointers for the first blocks of A and B.
+        # We will advance this pointer as we move in the K direction
+        # and accumulate
+        # `a_ptrs` is a block of [BLOCK_SIZE_M, BLOCK_SIZE_K] pointers
+        # `b_ptrs` is a block of [BLOCK_SIZE_K, BLOCK_SIZE_N] pointers
+        # See above `Pointer Arithmetic` section for details
+        offs_am = (pid_m * BLOCK_SIZE_M + np.arange(0, BLOCK_SIZE_M)) % M
+        offs_bn = (pid_n * BLOCK_SIZE_N + np.arange(0, BLOCK_SIZE_N)) % N
+        offs_k = tl.arange(0, BLOCK_SIZE_K)
+        a_ptrs = a_ptr + (offs_am[:, None] * stride_am + offs_k[None, :] * stride_ak)
+        b_ptrs = b_ptr + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
+
+        # -----------------------------------------------------------
+        # Iterate to compute a block of the C matrix.
+        # We accumulate into a `[BLOCK_SIZE_M, BLOCK_SIZE_N]` block
+        # of fp32 values for higher accuracy.
+        # `accumulator` will be converted back to fp16 after the loop.
+        accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+        for k in range(0, tl.cdiv(K, BLOCK_SIZE_K)):
+            # Load the next block of A and B, generate a mask by checking the K dimension.
+            # If it is out of bounds, set it to 0.
+            a = tl.load(a_ptrs, mask=offs_k[None, :] < K - k * BLOCK_SIZE_K, other=0.0)
+            b = tl.load(b_ptrs, mask=offs_k[:, None] < K - k * BLOCK_SIZE_K, other=0.0)
+            # We accumulate along the K dimension.
+            accumulator = tl.dot(a, b, accumulator)
+            # Advance the ptrs to the next K block.
+            a_ptrs += BLOCK_SIZE_K * stride_ak
+            b_ptrs += BLOCK_SIZE_K * stride_bk
